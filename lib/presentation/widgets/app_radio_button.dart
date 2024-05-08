c157ff3e0be12acd3625bc203df9a18a6e4b0ad9
dianichandra <chandradiani@gmail.com>
2024-05-08 16:51:02 +0800

    add bs picker

---
+++ lib/presentation/widgets/app_radio_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:hino_driver_app/infrastructure/theme/app_color.dart';
+
+class CustomRadioButton extends StatelessWidget {
+  final bool isSelected;
+  final VoidCallback onSelect;
+
+  CustomRadioButton({required this.isSelected, required this.onSelect});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onSelect,
+      child: Container(
+        width: 20,
+        height: 20,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: isSelected ? PrimaryColor.main : BorderColor.disabled,
+            width: 1,
+          ),
+        ),
+        child: isSelected
+            ? Center(
+                child: Container(
+                  width: 10, 
+                  height: 10, 
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: PrimaryColor.main,
+                  ),
+                ),
+              )
+            : null,
+      ),
+    );
+  }
+}
