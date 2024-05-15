4102c9fa3d2463b26d6aa742ec58fff449531434
unknown <maulidi.adiprasetia@gmail.com>
2024-05-15 09:18:10 +0800

    finish implement trip detail layout

--- lib/infrastructure/constants.dart
+++ lib/infrastructure/constants.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hino_driver_app/domain/core/entities/model.dart';
+import 'package:hino_driver_app/domain/core/entities/trips_model.dart';
 import 'package:hino_driver_app/presentation/widgets/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -15,6 +17,7 @@ class ButtonShapeValue {
 class Constants {
   static final MAP_API_KEY = "AIzaSyAzc3CqLAKvVzyciztdOcADxafOs1iYHbs";
   static final DATE_FORMAT_TZ = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+  static final DATE_FORMAT_PENALTY = "dd MMM yyyy, HH:mm";
 
   static final bottomMainMenu = [
     BottomNavigationBarItem(
@@ -71,4 +74,35 @@ class Constants {
     PickerModel(id: 2, title: 'Accident', value: 'accident'),
     PickerModel(id: 3, title: 'External Event', value: 'external_event'),
   ];
+
+  static final tripDetailData = TripModel(
+    origin: LatLng(-8.681547132266411, 115.24069589508952),
+    destination: LatLng(-8.677846354619318, 115.23787020063237),
+    penalties: [
+      PenaltyModel(
+        id: 1,
+        coordinate: LatLng(-8.680087825062431, 115.24217508151388),
+        type: PenaltyType.brake,
+        datetime: "2023-07-20T01:09:49.000000Z",
+      ),
+      PenaltyModel(
+        id: 2,
+        coordinate: LatLng(-8.677138607855436, 115.2396055095573),
+        type: PenaltyType.over_speed,
+        datetime: "2023-07-20T01:09:49.000000Z",
+      ),
+      PenaltyModel(
+        id: 3,
+        coordinate: LatLng(-8.677677058199146, 115.24235824877428),
+        type: PenaltyType.acceleration,
+        datetime: "2023-07-20T01:09:49.000000Z",
+      ),
+      PenaltyModel(
+        id: 4,
+        coordinate: LatLng(-8.67707191777169, 115.24227885415357),
+        type: PenaltyType.lateral_accel,
+        datetime: "2023-07-20T01:09:49.000000Z",
+      ),
+    ],
+  );
 }
