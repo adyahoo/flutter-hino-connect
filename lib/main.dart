37904a9e556afe6b6d5fc4dd379718bf9364d1ac
dianichandra <chandradiani@gmail.com>
2024-06-27 10:20:52 +0800

    fix: local data still there after fresh install

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localization/flutter_localization.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:get/get.dart';
 import 'package:hino_driver_app/infrastructure/di.dart';
 import 'package:hino_driver_app/infrastructure/theme/app_theme.dart';
 import 'package:hino_driver_app/infrastructure/translation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'infrastructure/navigation/navigation.dart';
 import 'infrastructure/navigation/routes.dart';
@@ -16,7 +18,8 @@ import 'package:timezone/data/latest_all.dart' as tz;
 
 late AndroidNotificationChannel channel;
 late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey =
+    GlobalKey<ScaffoldMessengerState>();
 
 Future<void> setupNotification() async {
   channel = const AndroidNotificationChannel(
@@ -39,15 +42,58 @@ Future<void> setupNotification() async {
   // }
 
   // await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.requestNotificationsPermission();
-  await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
 }
 
 void registerNotification() async {
   await setupNotification();
 }
 
+Future<void> clearSecureStorageOnReinstall() async {
+  const String key = 'hasRunBefore';
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  final hasRunBefore = prefs.getBool(key) ?? false;
+  debugPrint('Has run before: $hasRunBefore');
+
+  if (!hasRunBefore) {
+    debugPrint(
+        'First run detected, clearing secure storage and shared preferences...');
+    final FlutterSecureStorage storage = const FlutterSecureStorage();
+
+    try {
+      await storage.deleteAll();
+      debugPrint('Secure storage cleared.');
+    } catch (e) {
+      debugPrint('Error clearing secure storage: $e');
+
+      // Handling specific error by reinitializing storage
+      if (e.toString().contains('Failed to unwrap key')) {
+        await storage.deleteAll(); // Re-attempt to clear storage
+        debugPrint(
+            'Re-attempted to clear secure storage after key unwrap failure.');
+      }
+    }
+
+    // Clear SharedPreferences
+    await prefs.clear();
+    debugPrint('Shared preferences cleared.');
+
+    // Set flag to indicate the app has run before
+    await prefs.setBool(key, true);
+    debugPrint('Flag set.');
+  } else {
+    debugPrint(
+        'App has run before, not clearing secure storage or shared preferences.');
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await clearSecureStorageOnReinstall();
   var initialRoute = await Routes.initialRoute;
 
   tz.initializeTimeZones();
@@ -74,7 +120,8 @@ class Main extends StatelessWidget {
         Locale('en', 'US'),
         Locale('id', 'ID'),
       ],
-      localizationsDelegates: FlutterLocalization.instance.localizationsDelegates,
+      localizationsDelegates:
+          FlutterLocalization.instance.localizationsDelegates,
       initialRoute: initialRoute,
       getPages: Nav.routes,
     );
