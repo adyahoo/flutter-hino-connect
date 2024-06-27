f29b87e7998f4a02231aecd3ea8ca9156877a755
dianichandra <chandradiani@gmail.com>
2024-06-27 10:20:28 +0800

    fix: filter nearby search api response

--- lib/data/data_sources/place_data_source.dart
+++ lib/data/data_sources/place_data_source.dart
@@ -1,18 +1,23 @@
 part of 'data_source.dart';
 
 class PlaceDataSource {
-  final String baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+  final String baseUrl =
+      'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
   final double cacheRadius = 400;
 
-  Future<PlacesApiResponse>getPlaceList(double lat, double long, String type) async {
+  Future<PlacesApiResponse> getPlaceList(
+      double lat, double long, String type) async {
     final LatLng venueLocation = Constants.venueLocation;
-    final distance = calculateDistance(lat, long, venueLocation.latitude, venueLocation.longitude);
+    final distance = calculateDistance(
+        lat, long, venueLocation.latitude, venueLocation.longitude);
 
     // Check if it's within the cache radius
-    if (distance <= cacheRadius && (type != 'car_dealer' && type != 'service_center')) {
+    if (distance <= cacheRadius &&
+        (type != 'car_dealer' && type != 'service_center')) {
       print('within venue location radius');
       // Check if cached data is available
-      final cachedPlacesJson = await inject<StorageService>().getJsonData(StorageService.PLACE_NEAR_VENUE_JSON);
+      final cachedPlacesJson = await inject<StorageService>()
+          .getJsonData(StorageService.PLACE_NEAR_VENUE_JSON);
       print('cachedPlacesJson: $cachedPlacesJson');
       if (cachedPlacesJson?['results'].length != 0) {
         print('Using cached response');
@@ -25,16 +30,19 @@ class PlaceDataSource {
     print('Fetching new data');
     // Fetch data from the API
     final apiKey = Constants.MAP_API_KEY;
-    final apiUrl = '$baseUrl?location=$lat,$long&radius=400&type=$type&key=$apiKey';
-        // final apiUrl = '$baseUrl?location=$lat,$long&radius=400&type=$type&key=$apiKey&fields=address_component,geometry,formatted_address,name,types';
+    // final apiUrl = '$baseUrl?location=$lat,$long&radius=400&type=$type&key=$apiKey';
+    final apiUrl =
+        '$baseUrl?location=$lat,$long&radius=400&type=$type&key=$apiKey&fields=address_component,geometry,formatted_address,name,types';
     final response = await http.get(Uri.parse(apiUrl));
+
+    print('Response now perlu: ${response.body}');
 
     if (response.statusCode == 200) {
       final decodedData = json.decode(response.body);
 
-
       // Cache the fetched data if it's within the cache radius
-      if (distance <= cacheRadius && (type != 'car_dealer' && type != 'service_center')) {
+      if (distance <= cacheRadius &&
+          (type != 'car_dealer' && type != 'service_center')) {
         inject<StorageService>().setJsonData(
           StorageService.PLACE_NEAR_VENUE_JSON,
           decodedData,
