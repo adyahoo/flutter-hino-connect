99e45fec936f2e4100432c60dd26a652855f4a37
Maul-Mobile <maulidy.ady123@gmail.com>
2024-07-11 13:51:42 +0800

    fix:update event card external type

--- lib/presentation/widgets/app_card_action.dart
+++ lib/presentation/widgets/app_card_action.dart
@@ -1,8 +1,9 @@
 part of 'widgets.dart';
 
 class AppCardAction extends StatelessWidget {
-  const AppCardAction({super.key, required this.onEdit, required this.onDelete});
+  const AppCardAction({super.key, this.isEditable = true, required this.onEdit, required this.onDelete});
 
+  final bool isEditable;
   final Function() onEdit;
   final Function() onDelete;
 
@@ -10,15 +11,17 @@ class AppCardAction extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        AppButton.icon(
-          size: AppButtonSize.smallSize,
-          icon: Iconsax.edit_2,
-          isFullWidth: false,
-          label: "edit".tr,
-          onPress: onEdit,
-          type: AppButtonType.text,
-        ),
-        const SizedBox(width: 16),
+        if (isEditable) ...[
+          AppButton.icon(
+            size: AppButtonSize.smallSize,
+            icon: Iconsax.edit_2,
+            isFullWidth: false,
+            label: "edit".tr,
+            onPress: onEdit,
+            type: AppButtonType.text,
+          ),
+          const SizedBox(width: 16),
+        ],
         AppButton.icon(
           size: AppButtonSize.smallSize,
           icon: Iconsax.trash,
@@ -26,7 +29,6 @@ class AppCardAction extends StatelessWidget {
           label: "delete".tr,
           onPress: onDelete,
           type: AppButtonType.text,
-
         ),
       ],
     );
